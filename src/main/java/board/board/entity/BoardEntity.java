eb56b15ad8b14560e82ede2be00f32bd95f9a934
seoulit7 <seoulit@gnu.ac.kr>
2023-02-23 12:50:29 +0900

    test1

---
+++ src/main/java/board/board/entity/BoardEntity.java
@@ -0,0 +1,54 @@
+package board.board.entity;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="t_jpa_board")
+@NoArgsConstructor
+@Data
+public class BoardEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int boardIdx;
+
+	private String title;
+	
+
+	private String contents;
+	
+
+	private int hitCnt = 0;
+	
+
+	private String creatorId;
+	
+	@Column(nullable=false)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime createdDatetime = LocalDateTime.now();
+	
+	private String updaterId;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime updatedDatetime;
+
+	@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	@JoinColumn(name="board_idx")
+	private Collection<BoardFileEntity> fileList;
+}
---
+++ src/main/java/board/board/repository/JpaBoardRepository.java
@@ -0,0 +1,20 @@
+package board.board.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import board.board.entity.BoardEntity;
+import board.board.entity.BoardFileEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaBoardRepository extends CrudRepository<BoardEntity,Integer>{
+
+	List<BoardEntity> findAllByOrderByBoardIdxDesc();
+	List<BoardEntity> findBoardByUpdatedDatetimeStartBetweenByBoardIdxDesc(String title, String contents);
+	@Query("SELECT file FROM BoardFileEntity file WHERE board_idx = :boardIdx AND idx = :idx")
+	BoardFileEntity findBoardFile(@Param("boardIdx") int boardIdx, @Param("idx") int idx);
+}
